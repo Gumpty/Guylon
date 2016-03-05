3352e5a6aa2c9296dbd44557cd56d05d20531e57
Gumpty <gupter@gmail.com>
2016-03-05 21:45:13 +0000

    added express

--- hello.js
+++ hello.js
@@ -13,6 +13,19 @@ Cylon.robot({
     pca9685: { driver: 'pca9685' }
   },
 
+  events: ['turned_on', 'turned_off'],
+
+  // These are the commands that will be availble in the API
+  // Commands method needs to return an object with the aliases
+  // to the robot methods.
+  commands: function() {
+    return {
+      turn_on: this.turnOn,
+      turn_off: this.turnOff,
+      toggle: this.toggle
+    };
+  },
+
   work: function() {
     // for this example with sockets
     // we are going to be interacting
