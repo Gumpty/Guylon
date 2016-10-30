b17522f1e270e4c93f5929e382d4bcb74247d8ab
Gumpty <gupter@gmail.com>
2016-10-30 16:49:33 +0000

    Servo

--- guylon.js
+++ guylon.js
@@ -17,6 +17,10 @@ Cylon.robot({
             turn_off: this.turnOff,
             toggle: this.toggle,
             echo: this.echo,
+            setPWM: this.setPWM,
+            setPWMServoPulseMs: this.setPWMServoPulseMs, // For servos
+            setPWMFreq: this.setPWMFreq,
+            stop: this.stop,
         };
     },
 
@@ -27,12 +31,45 @@ Cylon.robot({
     devices: {
         //motor: { driver: 'motor', pin: 32 },
         //motor2: { driver: 'motor', pin: 33 }
+        pca9685: { driver: 'pca9685' }
     },
 
-    work: function () {
+    // Just for reference
+    pwmLimits: {
+        min: 0,
+        max: 4096,
+        freqMin: 40,
+        freqMax: 1000,
+    },
+
+    pwmFreq: 400,
+
+    work: function (my) {
         try {
             //this.motor.turnOff();
             //this.motor2.turnOff();
+
+            // var min = 700;
+            // var max = 1100;
+
+            // // set the frequency to 50hz
+            //my.pca9685.setPWMFreq(pwmFreq);
+            my.setPWMFreq(my.pwmFreq);
+            // reset and stop all outputs
+            my.pca9685.stop();
+
+            // // rotate to and hold the minium position
+            // my.pca9685.setPWM(0, 0, min);
+
+            // after((5).seconds(), function() {
+            //     // rotate to and hold the maxium position
+            //     my.pca9685.setPWM(0, 0, max);
+            // });
+
+            // after((10).seconds(), function() {
+            //     // reset and stop all outputs
+            //     my.pca9685.stop();
+            // });
         }
         catch (err) {
             console.log(err);
@@ -78,7 +115,74 @@ Cylon.robot({
         catch (err) {
             console.log(err);
         }  
+    },
+
+    setPWM: function (index, value) {
+        try {
+            if( value < this.pwmLimits.min ) { value = this.pwmLimits.min; }
+            if( value > this.pwmLimits.max ) { value = this.pwmLimits.max; }
+            if( index < 0 ) { index = 0; }
+            if( index > 16 ) { index = 16; }
+
+            this.pca9685.setPWM(index, 0, value);
+            return "PWM for index " + index + " set to " + value;
+        }
+        catch (err) {
+            console.log(err);
+        }
+    },
+
+    //servoMin: 160, // MG90D at 50Hz
+//servoMax: 490, // MG90D at 50Hz
+// 50 hz
+//    tick = 1000 / 50 = 20ms
+//    tickfraction = 160 / 4096 = 0.04
+//    ms = tick * tickfraction = 0.78
+
+//0.8ms min
+//2.4ms max
+
+//ms = (1000 / freq) * ( pwm / 4096 )
+//ms / (tick) = pwm / 4096
+//ms / tick * 4096 = pwm
+
+    setPWMServoPulseMs: function (index, ms) {
+        try {
+            var tickms = 1000 / this.pwmFreq;
+            var pwm = ms / tickms * 4096;
+
+            return this.setPWM(index, pwm);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    },
+
+    setPWMFreq: function (value) {
+        try {
+            if( value < this.pwmLimits.freqMin ) { value = this.pwmLimits.freqMin; }
+            if( value > this.pwmLimits.freqMax ) { value = this.pwmLimits.freqMax; }
+
+            this.pwmFreq = value;
+            this.pca9685.setPWMFreq(this.pwmFreq);
+            return "PWM freq set to " + this.pwmFreq;
+        }
+        catch (err) {
+            console.log(err);
+        }
+    },
+    
+
+    stop: function () {
+        try {
+            this.pca9685.stop();
+            return "PWM stopped";
+        }
+        catch (err) {
+            console.log(err);
+        }
     }
+
 });
 
 // ensure you install the API plugin first:
